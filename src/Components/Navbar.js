6de963706136a8aa26da01e381b1d50b8aa0d2e4
Henry Rayner <henryrayner@Henrys-MacBook-Pro-3.local>
2023-05-11 20:34:55 -0400

    editing links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -55,12 +55,12 @@ const Navigation = (props) => {
     return(
             <Navbar expand="lg" className="custom">
                 <Container className="container-fluid">
-                <Navbar.Brand href="/all-stars" className='logo'><img src={require("../Images/rupaul-logo.png")} width="55px" className='logo'/></Navbar.Brand>
+                <Navbar.Brand href="/" className='logo'><img src={require("../Images/rupaul-logo.png")} width="55px" className='logo'/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="container-fluid">
-                    <Nav.Link href="/all-stars" className="lipstick"><Lipstick text="HOME"/></Nav.Link>
-                    {isLoggedIn && <Nav.Link href={currUser.draftOrder.length !== 0 ? `/all-stars/${currUser.username}-selection`:"/all-stars/makeSelection"} className="lipstick"><Lipstick text="MY RANKING"/></Nav.Link>}
+                    <Nav.Link href="/" className="lipstick"><Lipstick text="HOME"/></Nav.Link>
+                    {isLoggedIn && <Nav.Link href={currUser.draftOrder.length !== 0 ? `#/${currUser.username}-selection`:"#/makeSelection"} className="lipstick"><Lipstick text="MY RANKING"/></Nav.Link>}
                     <Nav.Link href="" className="lipstick" disabled={true}><Lipstick text="LEADERBOARD"/></Nav.Link>
                     {LogInButton}
                     {AccountDropdown}
